77132b74216c81b4437251bdba081fbd731b4a74
Khan Bifov <abifov@smass.tech>
2020-03-19 10:49:18 +0400

    add description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import commander from 'commander'
 
 const { program } = commander
 
-program.version('0.1.0')
-
-program.parse(process.argv)
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .parse(process.argv)
