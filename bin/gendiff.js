1af3f63b8f69224be146534ddf4296b2052b9048
Khan Bifov <abifov@smass.tech>
2020-03-19 11:33:11 +0400

    add --format option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,4 +7,5 @@ const { program } = commander
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format')
   .parse(process.argv)
