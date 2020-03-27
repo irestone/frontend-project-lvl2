b8c13cc0ebe8c8fc5d9a644d61d4f84d6e0c6c6d
Khan Bifov <abifov@smass.tech>
2020-03-27 10:57:10 +0400

    add ini support

--- __tests__/index.js
+++ __tests__/index.js
@@ -7,31 +7,9 @@ const getFixturePath = (filename) => {
 }
 
 describe('gendiff', () => {
-  test('json', async () => {
-    const beforePath = getFixturePath('before.json')
-    const afterPath = getFixturePath('after.json')
-    const diff = gendiff(beforePath, afterPath)
-
-    expect(diff).toMatch(/^{\n( {2}( |\+|-) \w+: .+\n)*}$/gm)
-
-    const lines = diff.split('\n')
-
-    expect(lines).toHaveLength(7)
-
-    expect(lines).toContain('    d: d')
-    expect(lines).toContain('  - b: b')
-    expect(lines).toContain('  + c: c')
-    expect(lines).toContain('  - a: a')
-    expect(lines).toContain('  + a: aa')
-
-    expect(Math.abs(
-      lines.indexOf('  - a: a') - lines.indexOf('  + a: aa')
-    )).toBe(1)
-  })
-
-  test('yaml', async () => {
-    const beforePath = getFixturePath('before.yaml')
-    const afterPath = getFixturePath('after.yaml')
+  test.each(['json', 'yaml', 'ini'])('%s', (ext) => {
+    const beforePath = getFixturePath(`before.${ext}`)
+    const afterPath = getFixturePath(`after.${ext}`)
     const diff = gendiff(beforePath, afterPath)
 
     expect(diff).toMatch(/^{\n( {2}( |\+|-) \w+: .+\n)*}$/gm)
--- src/parser.js
+++ src/parser.js
@@ -1,9 +1,11 @@
 import yaml from 'js-yaml'
+import ini from 'ini'
 
 const parse = (content, format) => {
   switch (format) {
     case 'json': return JSON.parse(content)
     case 'yaml': return yaml.safeLoad(content)
+    case 'ini': return ini.parse(content)
     default: throw new Error(`Unknown format ${format}`)
   }
 }
