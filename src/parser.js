cea6605cef64fe79bd33feba6bd500e9a07b3ec1
Khan <khan.bifov@gmail.com>
2020-03-31 09:17:45 +0400

    add parser

--- src/parser.js
+++ src/parser.js
@@ -1,13 +1,41 @@
+import fs from 'fs'
+import path from 'path'
+import { mapValues, isObject, isString } from 'lodash'
+
 import yaml from 'js-yaml'
 import ini from 'ini'
 
-const parse = (content, format) => {
+const parseFile = (filepath) => {
+  const parse = getParser(path.extname(filepath).slice(1))
+  return fs.readFileSync(filepath, 'utf8') |> parse
+}
+
+const getParser = (format) => {
   switch (format) {
-    case 'json': return JSON.parse(content)
-    case 'yaml': return yaml.safeLoad(content)
-    case 'ini': return ini.parse(content)
+    case 'json': return JSON.parse
+    case 'yaml': return yaml.safeLoad
+    case 'ini': return iniParse
     default: throw new Error(`Unknown format ${format}`)
   }
 }
 
-export default parse
+const iniParse = (content) => {
+  const parsed = ini.parse(content)
+
+  const normalize = (obj) => {
+    return mapValues(obj, (value) => {
+      return isObject(value)
+        ? normalize(value)
+        : isString(value) && value.match(/^\d+$/)
+          ? Number(value)
+          : value
+    })
+  }
+
+  return normalize(parsed)
+}
+
+export {
+  parseFile,
+  getParser
+}
